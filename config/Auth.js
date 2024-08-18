61b66682d0a8b0a55a9922395a939578adf95147
Azmath Ali <mohdazmathali20@gmail.com>
2024-08-18 16:40:51 +0530

    Ready to deploy

---
+++ config/Auth.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv"
+import jwt from "jsonwebtoken"
+
+dotenv.config({
+    path:"../config/.env"
+})
+
+
+export const isAuthenticated = async (req, res, next)=>{
+
+    try {
+        const token = req.cookies.token
+        
+        if(!token){
+            return res.json({
+                message : "User is not Authenticated",
+                success : false
+            })
+        }
+        const decode = await  jwt.verify(token, process.env.TOKEN_SECRET)
+        
+        req.user = decode.userid
+        next()
+        
+    } 
+    catch (error) {
+        console.log(error)
+    }
+
+}
